386a2f82ec3a2c0dacfb62efbe862b798033efb3
Mariano Mendez <tuchorc@gmail.com>
2014-01-25 19:14:42 -0300

    Correccion de indentacon.

--- src/groovy/ar/com/burucps/party/PersonRoleTrader.groovy
+++ src/groovy/ar/com/burucps/party/PersonRoleTrader.groovy
@@ -4,16 +4,16 @@ import ar.com.burucps.library.create.ProductTrader;
 
 class PersonRoleTrader extends ProductTrader<PersonRole, String> {
 
-    static constraints = {
-    }
-	
+	static constraints = {
+	}
+
 	private static PersonRoleTrader INSTANCE = new PersonRoleTrader();
-	
-		// El constructor privado no permite que se genere un constructor por defecto.
-		// (con mismo modificador de acceso que la definición de la clase)
-		private PersonRoleTrader() {}
-	
-		public static PersonRoleTrader getInstance() {
-			return INSTANCE;
-		}
+
+	// El constructor privado no permite que se genere un constructor por defecto.
+	// (con mismo modificador de acceso que la definición de la clase)
+	private PersonRoleTrader() {}
+
+	public static PersonRoleTrader getInstance() {
+		return INSTANCE;
+	}
 }
