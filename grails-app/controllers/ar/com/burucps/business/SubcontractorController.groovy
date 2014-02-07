30fca513f761772a54b195daa7684e2a95f04d0d
Mariano Mendez <tuchorc@gmail.com>
2014-02-07 15:22:37 -0300

    Agrego comienzo para subagentes.

--- grails-app/controllers/ar/com/burucps/business/SubcontractorController.groovy
+++ grails-app/controllers/ar/com/burucps/business/SubcontractorController.groovy
@@ -1,6 +1,28 @@
 package ar.com.burucps.business
 
-class SubcontractorController {
+import ar.com.burucps.party.OrganizationCore
+import grails.transaction.Transactional
 
-    def index() { }
+@Transactional(readOnly = true)
+class SubcontractorController {
+	
+	static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+//	Data Binding to Multiple Domain Objects
+//	def album = new Album( params["album"] )
+//	def artist = new Artist( params["artist"] )
+//  Data Binding with the bindData Method
+//	The data-binding techniques you have seen so far are automatic and handled implicitly by Grails. However, in some circumstances you might need to exercise greater control over the data-binding process or to bind data to objects other than domain classes. To help tackle this issue, Grails provides a bindData method that takes the object to bind the data to and a java.util.Map.
+//	The map should contain keys that match the property names of the target properties within the passed object. As an example, if you wanted to ensure only the title property was bound to an Album instance, you could use the code shown in Listing 4-21.
+//	you can pass the Album instance as the first argument, and the parameters to bind to the instance as the second argument. The final argument is a map spec- ifying that you wish to include only the title property in the data-binding process. You could change the key within the map to exclude if you wished to bind all properties except the title property.
+//	def save = {
+//		def album = Album.get(params.id)
+//		 bindData(album, params, [include:"title"])
+//		 bindData(album, params, [include:"title"], "album")
+//	}
+//  <input type="text" name="artist.name" />
+
+    def index(Integer max) {
+		params.max = Math.min(max ?: 10, 100)
+		respond Subcontractor.list(params), model:[subcontractorInstanceCount: Subcontractor.count()]
+	}
 }
